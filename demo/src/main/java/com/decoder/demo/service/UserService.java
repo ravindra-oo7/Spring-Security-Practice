0446d781f3619015174d0478798046ecb9500ce8
ravindra-oo7 <ravindrashende7867@gmail.com>
2024-08-20 12:22:43 +0530

    BCrypt Password Conversion implementation

--- demo/src/main/java/com/decoder/demo/service/UserService.java
+++ demo/src/main/java/com/decoder/demo/service/UserService.java
@@ -1,6 +1,7 @@
 package com.decoder.demo.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.decoder.demo.model.Users;
@@ -12,8 +13,11 @@ public class UserService {
 	@Autowired
 	private UserRepo userRepo;
 	
+	private BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(12);
+	
 	public Users register(Users user)
 	{
+		user.setPassword(encoder.encode(user.getPassword()));
 		return userRepo.save(user);
 	}
 	
